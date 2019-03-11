8e537a220cbc75dbf0c9435d7b716d4f1ca0a658
Logan Leopold <loganleopld@me.com>
2019-03-11 17:23:51 -0400

    rebuilt structure from trash version

--- ticket_trackr_app/views.py
+++ ticket_trackr_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets          
+from .serializers import TestSerializer      
+from .models import TestModel
 
-# Create your views here.
+
+class TestView(viewsets.ModelViewSet):      
+    serializer_class = TestSerializer          
+    queryset = TestModel.objects.all()  
