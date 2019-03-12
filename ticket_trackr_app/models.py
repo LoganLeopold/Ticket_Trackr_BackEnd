0a8e2822ebaa134aab49310f7202c66ab5603271
Logan Leopold <loganleopld@me.com>
2019-03-12 15:42:04 -0400

    did a bit of writing trying to sort out thought process but really added model/serializer info

--- ticket_trackr_app/models.py
+++ ticket_trackr_app/models.py
@@ -5,3 +5,13 @@ from django.db import models
 class TestModel(models.Model):
     name = models.CharField(max_length=100)
     test = models.TextField()
+
+class FlightGet(models.Model):
+    inboundDate = models.CharField(max_length=10) 
+    cabinClass = models.TextField()
+    originPlace = models.TextField()
+    destinationPlace = models.TextField()
+    outboundDate = models.CharField(max_length=10)
+    adults = models.CharField(max_length=1)
+    # country = models.TextField()
+    # currency = models.TextField()
