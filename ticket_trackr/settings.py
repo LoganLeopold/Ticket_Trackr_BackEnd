65f3dbb8aac586d00eec1bc916a53f6ac7dcc6df
Logan Leopold <loganleopld@me.com>
2019-03-13 18:37:42 -0400

    got environment variables working at least for app secret key

--- ticket_trackr/settings.py
+++ ticket_trackr/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+from configparser import RawConfigParser
 import os
+from os import environ
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["TICKET_TRACKR_SECRET_KEY"]
+# config = RawConfigParser()
+# config.read('.settings.ini')
+
+# SECRET_KEY = config.get('secrets', 'TICKET_TRACKR_SECRET_KEY')
+
+SECRET_KEY = environ.get('TICKET_TRACKR_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- ticket_trackr_app/views.py
+++ ticket_trackr_app/views.py
@@ -3,12 +3,14 @@ from rest_framework import viewsets
 from .serializers import TestSerializer      
 from .models import TestModel
 import requests
+import os
 
+airportKey = os.environ.get('airportKey')
+airportAppId = os.environ.get('airportAppId')
 
 class TestView(viewsets.ModelViewSet):      
     serializer_class = TestSerializer          
     queryset = TestModel.objects.all()  
-
 
 
 def airport_list(request):
