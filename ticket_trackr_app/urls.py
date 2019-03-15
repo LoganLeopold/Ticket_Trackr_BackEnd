e5604f03d1c310eed0a9367df1169c6aef21dd3d
Logan Leopold <loganleopld@me.com>
2019-03-15 11:25:21 -0400

    had / before path in sub urls

--- ticket_trackr_app/urls.py
+++ ticket_trackr_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('/search', views.ListAirports),
+  path('search/', views.ListAirports),
 #   path('skyscan', views.GetLivePrices),
 #   path('skyscancollect', views.GetFormData)
 ]
