a9640afd078afa6145168aef6945f50c085a80ec
Logan Leopold <loganleopld@me.com>
2019-10-13 23:08:44 -0400

    got env var working by exporting into bashprof and just using termanal export command, then *restarting VSCODE*

--- ticket_trackr_app/views.py
+++ ticket_trackr_app/views.py
@@ -8,8 +8,7 @@ import json
 import requests
 import os
 
-RAPID_KEY = os.environ.get('RAPID')
-
+RAPID_KEY = os.environ.get("RAPIDAPI")
 
 class TestView(viewsets.ModelViewSet):
     serializer_class = TestSerializer
@@ -24,51 +23,35 @@ def CountryList(request):
     countries = CountryModel.objects.all()
     return render(request, 'countries.html', {'countries': countries})
 
-# put countries in the database upon deployment
-# def StoreCountries(request):
-
-#     payload = {
-#         'X-RapidAPI-Key': RAPID_KEY,
-#         'Content-Type': 'application/x-www-form-urlencoded'
-#     }
-
-#     response = requests.get(
-#         'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/countries/en-US', headers=payload)
-
-#     countries_data = response.json()
-
-#     for country in countries_data['Countries']:
-
-#     for country in countries_data['Countries']:
-#         country = CountryModel.objects.create(Name=country['Name'], Code=country['Code'])
-    
-#     return redirect('countrylist')
-
-    # return JsonResponse(countries_data, safe=False, content_type='text/html')
-
-# Update database every day
+#Get API response
 def CheckAPI(request):
 
-    payload = {
-        'X-RapidAPI-Key': RAPID_KEY,
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
+    url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/countries/en-US"
 
-    response = requests.get(
-        'https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/reference/v1.0/countries/en-US', headers=payload)
+    headers = {
+        'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+        'x-rapidapi-key': RAPID_KEY,
+        }
 
-    countries_data = response.json()
+    response = requests.request("GET", url, headers=headers)
 
-    query = CountryModel.objects.all()
+    print(RAPID_KEY)
+    data = response.json()
+    return JsonResponse(data, safe=False, content_type='text/html')
 
-    serializer = CountrySerializer(query, many=True)
 
-    for country in countries_data['Countries']:
-        print(country['Code'])
 
-    for country in serializer.data:
-        print(country['Code'])
+    # countries_data = response.json()
+    # return JsonResponse(request, safe=False, content_type='text/html')
 
-    return JsonResponse(serializer.data, safe=False, content_type='text/html')
+
+    # serializer = CountrySerializer(query, many=True)
+
+    # for country in countries_data['Countries']:
+    #     print(country['Code'])
+
+    # for country in serializer.data:
+    #     print(country['Code'])
+
+    # return JsonResponse(serializer.data, safe=False, content_type='text/html')
     
-
