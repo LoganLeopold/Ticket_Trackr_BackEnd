b9558aac8729e27d72d3e5235a5591795decfdbe
Logan Leopold <loganleopld@me.com>
2020-01-27 23:56:31 -0500

    closing back down countries save to db route

--- ticket_trackr_app/urls.py
+++ ticket_trackr_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
   path('countries/search/third', views.CheckAPI),
-  path('countries/db/save', views.saveCountries),
+  # path('countries/db/save', views.saveCountries),
   path('countries/search/db', views.CountryList, name='countrylist'),
 
   path('airports/search/third', views.checkAirportAPI),
